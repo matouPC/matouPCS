dfc7be8ce8fb047f3663312af37d5bed09c85b38
lbx <15136092338@163.com>
2017-03-13 09:26:19 +0800

    lbx3.13

--- Application/Runtime/Cache/Home/49b121750ad59544b32e7766dbc119bc.php
+++ Application/Runtime/Cache/Home/49b121750ad59544b32e7766dbc119bc.php
@@ -645,9 +645,8 @@
 				<div class="f-main-c">
 					<div class="f-c-1f">
 						
-						<p>© 2005－2016 douban.com, all rights reserved 北京豆网科技有限公司 </p>
-						<p>京ICP证090015号 京ICP备11027288号 网络视听许可证0110418号 </p>
-						<p>京网文[2015]2026-368号 京公网安备11010502000728 新出网证(京)字129号 </p>
+						<p>© 2016－2017 郑州码头网络技术有限公司 </p>
+						<p><a href="http://www.miitbeian.gov.cn">豫ICP备16015506号</a></p>
 					</div>
 				</div>
 			</div>
